67e077bb05a82da7525d659ff26fd771a419a899
MasterUNG <phrombutr@gmail.com>
2018-03-21 14:27:58 +0700

    Create RegisterFragment Success

--- app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/MainFragment.java
@@ -3,9 +3,11 @@ package masterung.androidthai.in.th.ungreadcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import masterung.androidthai.in.th.ungreadcode.R;
 
@@ -14,6 +16,33 @@ import masterung.androidthai.in.th.ungreadcode.R;
  */
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+
+
+
+    }   // Main Method
 
     @Nullable
     @Override
--- app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package masterung.androidthai.in.th.ungreadcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.ungreadcode.MainActivity;
 import masterung.androidthai.in.th.ungreadcode.R;
 
 /**
@@ -15,6 +17,30 @@ import masterung.androidthai.in.th.ungreadcode.R;
 
 public class RegisterFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+
+//        Show Navigator Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }   // Main Method
 
     @Nullable
     @Override
