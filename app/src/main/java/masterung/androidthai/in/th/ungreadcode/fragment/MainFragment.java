ca80007ff3e54b05bbe8ccb35e2c0bbc48ff09d1
sutitaf <comclassmkk@gmail.com>
2018-03-22 15:55:17 +0700

    Authen OK

--- app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/fragment/MainFragment.java
@@ -7,9 +7,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import masterung.androidthai.in.th.ungreadcode.R;
+import masterung.androidthai.in.th.ungreadcode.utility.GetAllUser;
+import masterung.androidthai.in.th.ungreadcode.utility.MyAlert;
+import masterung.androidthai.in.th.ungreadcode.utility.MyConstant;
 
 /**
  * Created by masterung on 20/3/2018 AD.
@@ -23,6 +32,99 @@ public class MainFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Register Controller
+        registerController();
+
+//        Login Controller
+
+        loginController();
+
+
+    }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.have_space),
+                            getString(R.string.message_have_spece));
+
+                } else {
+//                    No Space
+                    try {
+
+                        MyConstant myConstant = new MyConstant();
+                        GetAllUser getAllUser = new GetAllUser(getActivity());
+                        getAllUser.execute(myConstant.getUrlGetAllUserString());
+
+                        String jsonString = getAllUser.get();
+                        Log.d("22MarchV1", "JSON ==>" + jsonString);
+
+                        String[] columUserStrings = myConstant.getLoginStrings();
+                        String[] loginStrings = new String[columUserStrings.length];
+                        boolean statusBool = true;
+
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+
+                        for (int i = 0; i < jsonArray.length();  i+=1) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                            if (userString.equals(jsonObject.getString(columUserStrings[2]))) {
+
+                                statusBool = false;
+                                for (int i1=0; i1<columUserStrings.length; i1+=1) {
+
+                                    loginStrings[i1] = jsonObject.getString(columUserStrings[i1]);
+                                    Log.d("22MarchV1", "loginString[" + i1 + "]==>" + loginStrings[i1]);
+                                }
+
+                            } // if
+
+                        }  // for
+
+                        if (statusBool) {
+//                            user false
+
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("User False",
+                                    "No This User in mySQL");
+
+                        } else if (passwordString.equals(loginStrings[3])) {
+//                            Password True
+                            Toast.makeText(getActivity(), "Welcom" + loginStrings[1], Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+//                            Password False
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("Password False", "Please Try Again Password False");
+                        }
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                } // If
+
+            } // onClick
+        });
+    }
+
+    private void registerController() {
         TextView textView = getView().findViewById(R.id.txtRegister);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,11 +140,7 @@ public class MainFragment extends Fragment{
 
             }
         });
-
-
-
-
-    }   // Main Method
+    }
 
     @Nullable
     @Override
--- app/src/main/java/masterung/androidthai/in/th/ungreadcode/utility/MyConstant.java
+++ app/src/main/java/masterung/androidthai/in/th/ungreadcode/utility/MyConstant.java
@@ -7,8 +7,19 @@ package masterung.androidthai.in.th.ungreadcode.utility;
 public class MyConstant {
 
     //    About URL
+    private String urlGetAllUserString = "http://androidthai.in.th/mar/getAllUser.php";
     private String urlPostUserString = "http://androidthai.in.th/mar/postUser.php";
 
+    //    About Array
+    private String[] loginStrings = new String[]{"id", "Name", "User", "Password"};
+
+    public String[] getLoginStrings() {
+        return loginStrings;
+    }
+
+    public String getUrlGetAllUserString() {
+        return urlGetAllUserString;
+    }
 
     public String getUrlPostUserString() {
         return urlPostUserString;
